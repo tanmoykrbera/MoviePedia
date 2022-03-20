38d59d15a120e178d668d0d7e90e025169f7caf8
tanmoykrbera <tanmoysunny3@gmail.com>
2022-03-20 05:30:00 +0530

    moviepedia commit

--- resources/views/search_movie.blade.php
+++ resources/views/search_movie.blade.php
@@ -1,1 +1,19 @@
- 
+@include('header')
+
+<div class="container" style="background-color:#232323" id="searchMovie" >
+   <h5 class="top-head" style="color:white">Popular On MoviePedia</h5> 
+   <div class="row">
+     
+       <div class="col-sm-2 mt-5" id="movieList" >
+         <div class="" id="searchMovie">  
+            <video width="520" height="340" controls>
+               <source src="/videos/The_road_trick.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+           </video>
+         </div>
+       
+        </div>    
+    
+   </div>     
+</div>    
+@include('footer') 
